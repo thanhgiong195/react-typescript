701b7d11de52cfd5b7b6608e459f357e0f5c799d
ThanhGiong <macbook@ThanhGiongMac.local>
2020-05-26 21:08:53 +0700

    add Context demo

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,10 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from './components/organism/header'
 
 import { Home } from './components/pages/home'
-import { login } from './components/pages/login'
-import { signUp } from './components/pages/signUp'
+import { Login } from './components/pages/login'
+import { SignUp } from './components/pages/signUp'
 
-import { SampleContext, useUpdateContext } from './useContext'
+import { SampleContext, useUpdateContext } from './userContext'
 
 axios.defaults.baseURL = 'https://5e75b1629dff12001635417e.mockapi.io';
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
@@ -22,8 +22,8 @@ export function App() {
                     <div className="App">
                         <Header />
                         <Switch>
-                            <Route exact path="/login" component={login} />
-                            <Route exact path="/signup" component={signUp} />
+                            <Route exact path="/login" component={Login} />
+                            <Route exact path="/signup" component={SignUp} />
                             <Route exact path="/" component={Home} />
                         </Switch>
                     </div>
